25f40d4bb001b8c15a0d5be3f80875dbc8cafccf
Paul Carrillo <paulcarrillo05@gmail.com>
2017-09-11 17:08:01 -0600

    initial git. State is now in the main app. BookList Component was created

---
+++ src/BookList.js
@@ -0,0 +1,11 @@
+  import React, { Component }from 'react'
+
+  class BookList extends Component {
+    render() {
+      return (
+        <div>This is the Book List!</div>
+      )
+    }
+  }
+
+  export default BookList;
